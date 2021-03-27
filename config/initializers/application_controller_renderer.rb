02323ff0eef6d9a048f2413834e204b288a4933e
snrk-m <mrt.bearpu3@gmail.com>
2021-03-27 08:05:51 +0000

    finish commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'a6ef2fc2db50c14b27a2c19f3b5d194f880a5a8a33e7364b2d145a15cfc84cd953bd905018e7b1390e3d2f0b478b6e2c70e0703237a539fc62caedca8521823b'
